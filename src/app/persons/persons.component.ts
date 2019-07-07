2ecd214775f5ba1b5840a7fe85d9da9b01c5d971
Elias de Francisco Javaloyes <eliasdefrancisco@gmail.com>
2019-07-07 11:28:27 +0200

    Observables

--- src/app/persons/persons.component.ts
+++ src/app/persons/persons.component.ts
@@ -1,17 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { PersonsService } from './persons.service';
 
 @Component({
   selector: 'app-persons',
   templateUrl: './persons.component.html'
 })
-export class PersonsComponent implements OnInit {
+export class PersonsComponent implements OnInit, OnDestroy {
   personList: string[];
+  private personListSubs: Subscription;
 
   constructor(private prsService: PersonsService) {}
 
   ngOnInit() {
     this.personList = this.prsService.persons;
+    this.personListSubs = this.prsService.personsChanged.subscribe(persons => this.personList = persons);
+  }
+
+  ngOnDestroy() {
+    this.personListSubs.unsubscribe();
   }
 
   onRemovePerson(name: string) {
